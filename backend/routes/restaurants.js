ee7e0702ccc8a23c5ece9ef7ca84cd1957f203ee
jeffzyliu <jeffzyliu@gmail.com>
2020-05-23 00:51:51 -0400

    added restaurant TODOs

--- backend/routes/restaurants.js
+++ backend/routes/restaurants.js
@@ -12,6 +12,50 @@ const { userLogin, ownerLogin } = require("../modules/login");
 const connection = require("../modules/sqlconnection");
 const db = config.database.database;
 
-// TODO finalizing the logic w/ frontend
+// TODO GET /restaurants/foods - get list of all foods and prices
+/*
+Send: {
+	(header)
+}
+Response: {
+	[ {FoodID, FoodName, FoodPrice}... ]
+}
+*/
+
+// TODO POST /restaurants/foods - add new foods to a restaurant’s list
+/*
+Send: {
+	(header),
+	FoodName: string,
+	FoodPrice: number
+}
+Insert into blah blah, use restaurantID in header
+Send back: {
+	Some confirmation stuff
+}
+*/
+
+// TODO PUT /restaurants/foods/:FoodID - update the price of a food
+/*
+Send: {
+	(header), 
+	NewPrice: number
+}
+Update Foods (FoodPrice) values (req.body.NewPrice) where FoodID = req.params.FoodID and RestaurantID = req.RestaurantID
+Response: {
+	Some happy go lucky success message
+} 
+*/
+
+// TODO DELETE /restaurants/foods/:FoodID -- take food item off the menu (put disguised as a delete)
+/*
+send: {
+	(header),
+}
+Update OnMenu to False in Foods where FoodID = req.paramsFoodID and RestaurantID = req.RestaurantID
+Send back: {
+	Some confirmation stuff
+}
+*/
 
 module.exports = restaurantsRouter;
