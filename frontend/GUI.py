27e4fc9fa6eb34760749d818ba554fe587bf3b3f
Celina Tala <celinatala@vpn-two-factor-general-230-131-125.dartmouth.edu>
2020-05-27 15:52:08 -0700

    Final User GUI

--- frontend/GUI.py
+++ frontend/GUI.py
@@ -8,107 +8,198 @@ from functools import partial
 from tkcalendar import DateEntry
 from datetime import datetime as dt
 
+#some global variables we need
 login_info = {}
-login_validation = True
-dictionary = {"HOP": ["Chicken Burger", "Grilled Cheese"], "Collis": ["Omelete", "Stir Fry"], "TukTuk": ["Pad Thai", "Fried Rice"]}
+restaurants = ["Courtyard Cafe", "Foco"]
+food_ID = {}
 restaurant = ""
 date = dt.today().strftime('%Y-%m-%d')
+url = "http://localhost:3000/"
 
 def back_selection():
     u.withdraw()
     user.deiconify()
 
-def foods(event):
-    global restaurant
-    restaurant = r.get()
-    
-def changeFoods():
-    food["values"] = dictionary[restaurant]
-
-def getdate(event):
-    global date
-    date = cal.get_date()
-
-def addmeal():
-    global r, food, u, cal
+def back_login():
+    global user
     user.withdraw()
-    u = Toplevel()
-    u.geometry('400x200')
-    Label(u, text = "Food Category").grid(row = 0, column = 0)
-    category = ttk.Combobox(u, values=["Breakfast", "Lunch", "Dinner", "Late Night"], state = "readonly").grid(row = 1, column = 0)
-    Label(u, text = "Restaurant").grid(row = 2, column = 0)
-    r = ttk.Combobox(u, values=dictionary.keys(), state = "readonly")
-    r.grid(row = 3, column = 0)
-    r.bind("<<ComboboxSelected>>", foods)
-    Label(u, text = "Foods").grid(row = 0, column = 1)
-    food = ttk.Combobox(u, values = dictionary["HOP"], state = "readonly", postcommand=changeFoods)
-    food.grid(row = 1, column = 1)
-    Label(u, text = "Date").grid(row = 2, column = 1)
-    cal = DateEntry(u)
-    cal.grid(row = 3, column = 1)
-    cal.bind("<<DateEntrySelected>>", getdate)
-    Button(u, text = "Add").grid(row = 4, column = 0)
-    Button(u, text = "Back", command = back_selection).grid(row = 4, column =1)
+    log.deiconify()
 
 def back2():
     global d
     d.withdraw()
     u.deiconify()
 
-def update_meals():
-    x = Toplevel()
-    Label(x, text = "Food Category").grid(row = 0, column = 0)
-    category = ttk.Combobox(x, values=["Breakfast", "Lunch", "Dinner", "Late Night"], state = "readonly").grid(row = 1, column = 0)
-    Label(x, text = "Restaurant").grid(row = 2, column = 0)
-    r = ttk.Combobox(x, values=dictionary.keys(), state = "readonly")
+def foods(event):
+    global restaurant
+    restaurant = r.get()
+    
+def changeFoods():
+    global food_ID
+    resp = requests.get(url+"foods/"+restaurant, json = login_info)
+    if resp.json()['status']!= 200:
+        messagebox.showerror("Error", "Error")
+    else:
+        food_list = []
+        for foods in resp.json()["response"]:
+            food_ID[foods["FoodName"]] = foods["FoodID"]
+            food_list.append(foods["FoodName"])
+        food["values"] = food_list
+
+def addFoods():
+    data = login_info.copy()
+    data["TransactionDate"]= date
+    data["TransactionCategory"]=category.get()
+    data["FoodID"]= food_ID[food.get()]
+    resp = requests.post(url+"meals", json = data)
+    if resp.json()["status"] != 201:
+        messagebox.showerror("Error", "Error")
+    else:
+        messagebox.showinfo("Added", "Meal Successfully Added")
+
+
+def getdate(event):
+    global date
+    date = cal.get_date()
+    date = date.strftime('%Y-%m-%d')
+
+def addmeal():
+    global r, food, u, cal, category
+    user.withdraw()
+    u = Toplevel()
+    u.geometry('400x200')
+    Label(u, text = "Food Category").grid(row = 0, column = 0)
+    category = ttk.Combobox(u, values=["Breakfast", "Lunch", "Dinner", "Late Night"], state = "readonly")
+    category.grid(row = 1, column = 0)
+    Label(u, text = "Restaurant").grid(row = 2, column = 0)
+    r = ttk.Combobox(u, values=restaurants, state = "readonly")
     r.grid(row = 3, column = 0)
     r.bind("<<ComboboxSelected>>", foods)
-    Label(x, text = "Foods").grid(row = 0, column = 1)
-    food = ttk.Combobox(x, values = dictionary["HOP"], state = "readonly", postcommand=changeFoods)
+    Label(u, text = "Foods").grid(row = 0, column = 1)
+    food = ttk.Combobox(u, values = "Nothing", state = "readonly", postcommand=changeFoods)
     food.grid(row = 1, column = 1)
-    Label(x, text = "Date").grid(row = 2, column = 1)
-    cal = DateEntry(x)
+    Label(u, text = "Date").grid(row = 2, column = 1)
+    cal = DateEntry(u)
     cal.grid(row = 3, column = 1)
     cal.bind("<<DateEntrySelected>>", getdate)
-    Button(x, text = "Update").grid(row = 4, column = 0)
+    Button(u, text = "Add", command = addFoods).grid(row = 4, column = 0)
+    Button(u, text = "Back", command = back_selection).grid(row = 4, column =1)
+
+def update_api():
+    data = login_info.copy()
+    id = food_ID[food.get()]
+    data["FoodID"]=id
+    resp = requests.put(url+"meals/"+str(Transactionid.get()), json = data)
+    if resp.json()["status"] != 201:
+        messagebox.showerror("error", "Error")
+    else:
+        messagebox.showinfo("Update", "Update Successful")
+
+def update_meals():
+    global foods, food, restaurant
+    user_foods={}
+    foods = []
+    for x in meal_dates:
+        if x["TransactionID"] == Transactionid.get():
+            user_foods = x 
+            break 
+    restaurant = user_foods["RestaurantName"]
+    x = Toplevel()
+    Label(x, text = "New Food").pack()
+    food = ttk.Combobox(x, values = "Foods", state = "readonly", postcommand=changeFoods)
+    food.pack()
+    Button(x, text = "Update", command = update_api).pack()
+
+def delete_api():
+    resp = requests.delete(url + "meals/"+str(Transactionid.get()), json=login_info)
+    if resp.json()["status"] != 201:
+        messagebox.showerror("error", "Error")
+    else:
+        messagebox.showinfo("Update", "Delete Successful")
 
 def meal_date(b):
-    global d, TransactioniD
+    global d, Transactionid, meal_dates
     u.withdraw()
     d = Toplevel()
-    d.geometry('400x300')
+    d.geometry('600x300')
     if b.get() == 2:
-        # resp = requests.get("url", json=login_info)
-        Label(d, text = "transactions").pack()
+        resp = requests.get(url+"expenditures", json=login_info)
+        resp2 = requests.get(url+"meals/", json=login_info)
+        if resp.json()["status"]!= 200 or resp2.json()["status"]!= 201:
+            messagebox.showerror("Error", "error")
+        else:
+            meals = resp2.json()["response"]
+            c2 = Frame(d, width = 500, height = 50)
+            Label(c2, text = "Date", width = 15).grid(row = 0, column = 0)
+            Label(c2, text = "Category", width = 15).grid(row = 0, column = 1)
+            Label(c2, text = "Restaurant", width =15).grid(row = 0, column = 2)
+            Label(c2, text = "Cost", width = 15).grid(row = 0, column = 3)
+            c2.pack()
+            container = ttk.Frame(d, width = 500)
+            canvas = tk.Canvas(container, width = 500)
+            scrollbar = ttk.Scrollbar(container, orient = "vertical", command = canvas.yview)
+            scrollable_frame = ttk.Frame(canvas, width = 500)
+            scrollable_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+            canvas.create_window((0,0), window=scrollable_frame, anchor="nw")
+            canvas.configure(yscrollcommand = scrollbar.set)
+            Transactionid = IntVar()
+            r = 1
+            for i in range(len(meals)):
+                Label(scrollable_frame, text=meals[i]["Date"], width = 15).grid(row = r, column = 0)
+                Label(scrollable_frame, text = meals[i]["MealCategory"], width = 15).grid(row = r, column = 1)
+                Label(scrollable_frame, text = meals[i]["RestaurantName"], width = 15).grid(row = r, column =2)
+                Label(scrollable_frame, text = meals[i]["Cost"], width = 15).grid(row = r, column = 3)
+                r+=1
+            container.pack()
+            c3 = Frame(d)
+            Label(c3, text = "Total Expenditures").grid(row = 5, column = 0)
+            Label(c3, text = resp.json()["TotalExpenditures"]).grid(row = 5, column = 1)
+            Button(c3, text = "Back", command = back2).grid(row = 6, column = 0)
+            canvas.pack(side = "left", fill="both", expand=True)
+            scrollbar.pack(side="right", fill="y")
+            c3.pack()
     else:
-        # url = url + date.strftime('%Y-%m-%d')
-        # resp = requests.get(url, json=login_info)
-        data = [{"TransactionID": 0, "TransactionCategory": "Breakfast", "RestaurantName": "FOCO", "FoodName": "Breakfast"}, {"TransactionID": 1, "TransactionCategory": "Lunch", "RestaurantName": "HOP", "FoodName": "Burger"}]
-        c2 = Frame(d, width = 300, height = 50)
+        meal_dates = []
+        if b.get() == 0:
+            resp = requests.get(url+"mealfoods", json = login_info)
+            if resp.json()["status"]!= 200:
+                messagebox.showerror("Error", "error")
+            else:
+                meal_dates = resp.json()["response"]
+        else:
+            resp=requests.get(url+"mealfoods/"+date, json = login_info)
+            if resp.json()["status"]!= 200:
+                messagebox.showerror("Error", "error")
+            else:
+                meal_dates = resp.json()["response"]
+        c2 = Frame(d, width = 500, height = 50)
         Label(c2, text = "Fetched Data").grid(row = 0, column = 3)
-        Label(c2, text = "", width = 5).grid(row = 1, column =1)
-        Label(c2, text = "Restaurant", width = 10).grid(row = 1, column = 2)
-        Label(c2, text = "Food Name", width = 10).grid(row = 1, column = 3)
-        Label(c2, text = "Date", width =10).grid(row = 1, column = 4)
+        Label(c2, text = "", width = 5).grid(row = 1, column =0)
+        Label(c2, text = "Restaurant", width = 15).grid(row = 1, column = 1)
+        Label(c2, text = "Food", width = 15).grid(row = 1, column = 2)
+        Label(c2, text = "Date", width =15).grid(row = 1, column = 3)
+        Label(c2, text = "Category", width = 15).grid(row = 1, column = 4)
         c2.pack()
-        container = ttk.Frame(d)
-        canvas = tk.Canvas(container)
+        container = ttk.Frame(d, width = 500)
+        canvas = tk.Canvas(container, width = 500)
         scrollbar = ttk.Scrollbar(container, orient = "vertical", command = canvas.yview)
-        scrollable_frame = ttk.Frame(canvas)
+        scrollable_frame = ttk.Frame(canvas, width = 500)
         scrollable_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
         canvas.create_window((0,0), window=scrollable_frame, anchor="nw")
         canvas.configure(yscrollcommand = scrollbar.set)
         Transactionid = IntVar()
         r = 2
-        for i in range(len(data)):
-            Radiobutton(scrollable_frame,variable = Transactionid, value = data[i]["TransactionID"]).grid(row = r, column = 0)
-            Label(scrollable_frame, text=data[i]["RestaurantName"], width = 10).grid(row = r, column = 1)
-            Label(scrollable_frame, text = data[i]["FoodName"], width = 10).grid(row = r, column = 2)
+        for i in range(len(meal_dates)):
+            Radiobutton(scrollable_frame,variable = Transactionid, value = meal_dates[i]["TransactionID"]).grid(row = r, column = 0)
+            Label(scrollable_frame, text=meal_dates[i]["RestaurantName"], width = 15).grid(row = r, column = 1)
+            Label(scrollable_frame, text = meal_dates[i]["FoodName"], width = 15).grid(row = r, column = 2)
+            Label(scrollable_frame, text = meal_dates[i]["Date"], width = 15).grid(row = r, column =3)
+            Label(scrollable_frame, text = meal_dates[i]["TransactionCategory"], width = 15).grid(row = r, column = 4)
             r+=1
         container.pack()
         c3 = Frame(d)
         Button(c3, text = "Update", command = update_meals).grid(row = 5, column = 0)
-        Button(c3, text = "Delete").grid(row = 5, column = 1)
+        Button(c3, text = "Delete", command = delete_api).grid(row = 5, column = 1)
         Button(c3, text = "Back", command = back2).grid(row = 5, column = 2)
         canvas.pack(side = "left", fill="both", expand=True)
         scrollbar.pack(side="right", fill="y")
@@ -145,6 +236,7 @@ def user_page():
     user.title("User Selection Page")
     Button (user, text = "Add a Meal",command = addmeal).pack(anchor = CENTER)
     Button(user, text = "View Transactions", command = get_meal).pack()
+    Button(user, text = "Back", command = back_login).pack()
 
 def addapi():
     data = login_info.copy()
@@ -152,6 +244,15 @@ def addapi():
     data["FoodPrice"] = price.get()
     resp = reponse.get("url", json = data)
     
+def add_food_api():
+    data = login_info.copy()
+    data["FoodName"] = name.get()
+    data["FoodPrice"] = price.get()
+    resp = requests.post(url+"restaurants/foods", json = data)
+    if resp.json()["status"] != 201:
+        messagebox.showerror("error", resp.json()["error"])
+    else:
+        messagebox.showinfo("Success", "Food Successfully Added")
 
 def add_food():
     global u, name, price
@@ -164,27 +265,73 @@ def add_food():
     Label(u, text = "Food Price").grid(row = 1, column = 0)
     price = DoubleVar()
     Entry(u, textvariable = price).grid(row = 1, column = 1)
-    Button(u, text = "Add").grid(row = 2, column = 0)
+    Button(u, text = "Add", command = add_food_api).grid(row = 2, column = 0)
     Button(u, text = "Back", command = back_selection).grid(row = 2, column = 1)
 
-def food_update():
-    global u
-    user.withdraw()
-    u = Toplevel()
-    u.geometry('400x200')
+def update_food_api():
+    data = login_info.copy()
+    data["NewPrice"] = price.get()
+    resp = requests.put(url+"restaurants/foods/" + str(Restaurant_foodID.get()), json = data)
+    if resp.json()["status"] != 200:
+        messagebox.showerror("error", resp.json()["error"])
+    else:
+        messagebox.showinfo("Success", "Food Price Successfully Updated")
+
+def update_food():
+    global updatefood, price
+    updatefood = Toplevel()
+    updatefood.geometry('400x100')
     #do another grid of radiobuttons
     price = DoubleVar()
-    Button(u, text = "Delete").pack()
-    Label(u, text = "New Price").pack()
-    Entry(u, textvariable = price).pack()
-    Button(u, text = "Update").pack()
-    Button(u, text = "Back", command = back_selection).pack()
+    Label(updatefood, text = "New Price").pack(side = LEFT)
+    Entry(updatefood, textvariable = price).pack(side = LEFT)
+    Button(updatefood, text = "Update", command = update_food_api).pack(anchor = W, side = BOTTOM)
 
-def view_transaction():
-    global u
+def delete_food_api():
+    resp = requests.delete(url+"restaurants/foods/"+str(Restaurant_foodID.get()), json = login_info)
+    if resp.json()["status"] != 200:
+        messagebox.showerror("error", resp.json()["error"])
+    else:
+        messagebox.showinfo("Success", "Food Successfully Deleted")
+
+def view_menu():
+    global u, Restaurant_foodID
     user.withdraw()
     u = Toplevel()
-    u.geometry('400x200')
+    u.geometry('400x400')
+    resp = requests.get(url+"restaurants/foods", json=login_info)
+    if resp.json()["status"] != 200:
+        messagebox.showerror("error", resp.json()["error"])
+    else:
+        menu_items=resp.json()["response"]
+        c2 = Frame(u, width = 300, height = 50)
+        Label(c2, text = "").grid(row = 0, column = 3)
+        Label(c2, text = "", width = 3).grid(row = 1, column =0)
+        Label(c2, text = "Food", width = 15).grid(row = 1, column = 1)
+        Label(c2, text = "Price", width = 15).grid(row = 1, column = 2)
+        c2.pack()
+        container = ttk.Frame(u, width = 300)
+        canvas = tk.Canvas(container, width = 300)
+        scrollbar = ttk.Scrollbar(container, orient = "vertical", command = canvas.yview)
+        scrollable_frame = ttk.Frame(canvas, width = 300)
+        scrollable_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+        canvas.create_window((0,0), window=scrollable_frame, anchor="nw")
+        canvas.configure(yscrollcommand = scrollbar.set)
+        Restaurant_foodID = IntVar()
+        r = 2
+        for i in range(len(menu_items)):
+            Radiobutton(scrollable_frame,variable = Restaurant_foodID, value = menu_items[i]["FoodID"]).grid(row = r, column = 0)
+            Label(scrollable_frame, text=menu_items[i]["FoodName"], width = 15).grid(row = r, column = 1)
+            Label(scrollable_frame, text = menu_items[i]["FoodPrice"], width = 15).grid(row = r, column = 2)
+            r+=1
+        container.pack()
+        c3 = Frame(u)
+        Button(c3, text = "Update", command = update_food).grid(row = 5, column = 0)
+        Button(c3, text = "Delete", command = delete_food_api).grid(row = 5, column = 1)
+        Button(c3, text = "Back", command = back_selection).grid(row = 5, column = 2)
+        canvas.pack(side = "left", fill="both", expand=True)
+        scrollbar.pack(side="right", fill="y")
+        c3.pack()
 
 
 def owner_page():
@@ -194,25 +341,23 @@ def owner_page():
     user.geometry('350x200')
     user.title("Owner Selection Page")
     Button(user, text = "Add Food", command = add_food).pack()
-    Button(user, text = "Delete food", command = food_update).pack()
-    Button(user, text = "Change Price", command = food_update).pack()
-    Button(user, text = "View Transactions", command = view_transaction).pack()
+    Button(user, text = "View Menu", command = view_menu).pack()
+    Button(user, text = "Back", command = back_login).pack()
 
 
 def validate(username, password, owner):
-    global login_validation
+    global login_info, ID_info
     info = {"isOwnerLogin": owner, "Username": username.get(), "Password": password.get()}
-    print(info)
-    # resp = requests.get(login, json=login_info)
-    if not login_validation:
+    resp = requests.get(url+"login", json=info)
+    if resp.json()['status']!= 200:
         messagebox.showerror("Error", "Failed to login")
     else:
-        login_validation={"Username":username.get(), "Password":password.get()}
+        login_info={"Username": username.get(), "Password": password.get()}
         if info["isOwnerLogin"] == 0:
             user_page()
         else:
-            # restaurantID = resp.json()['RestaurantID']
-            # login_validation["RestaurantID"] = restaurantID
+            restaurantID = resp.json()['RestaurantID']
+            login_info["RestaurantID"] = restaurantID
             owner_page()
 
 def delete(u, p):
@@ -248,15 +393,13 @@ def login(owner):
 
 def create_account(username, password, first, middle, last):
     data = {"Username": username.get(), "Password": password.get(), "FirstName": first.get(), "MiddleInitial": middle.get(), "LastName": last.get()}
-    # resp = requests.get("post/user", json = data)
-    print(data)
-    messagebox.showinfo("Success", "User Successfully Created")
-    # if resp.json()['status'] == 1000:
-    #     messagebox.showerror("Error", "Duplicate username")
-    # elif resp.json()['status'] == 500:
-    #     messagebox.showerror("Error", "Other Error")
-    # else:
-    #     messagebox.showinfo("Success", "User Successfully Created")
+    resp = requests.post("http://localhost:3000/register", json = data)
+    if resp.json()['status'] == 1000:
+        messagebox.showerror("Error", "Duplicate username")
+    elif resp.json()['status'] == 500:
+        messagebox.showerror("Error", "Other Error")
+    else:
+        messagebox.showinfo("Success", "User Successfully Created")
 
 def delete2(a, b, c, d, e):
     a.delete(0, END)
